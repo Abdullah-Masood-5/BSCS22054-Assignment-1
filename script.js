86564217af09b2461a4cb0a5e6d80b48c4428027
Abdullah Masood <bscs22054@itu.edu.pk>
2024-09-15 21:33:58 +0500

    Updated JS for better Nav in mobile view

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
- const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links a');
 
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
+// Toggle menu visibility on click of the menu icon (for mobile)
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Close the menu when a navigation link is clicked
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active'); // Close the menu
     });
+});
